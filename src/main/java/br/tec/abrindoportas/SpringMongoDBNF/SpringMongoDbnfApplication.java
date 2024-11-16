5428d945a2270e92cd32e75bd7820d2209aa0369
Edir P Santos <fennder@gmail.com>
2024-11-16 01:31:27 -0300

    First Commit for demonstration with new functionalities in Java Spring Boot

---
+++ src/main/java/br/tec/abrindoportas/SpringMongoDBNF/SpringMongoDbnfApplication.java
@@ -0,0 +1,13 @@
+package br.tec.abrindoportas.SpringMongoDBNF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDbnfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoDbnfApplication.class, args);
+	}
+
+}
